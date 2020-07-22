02f59b6c6da3b08c6059d20ce7216366e296b96d
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-23 01:53:58 +0530

    added login page activity to Androidmanifest and linked java file to xml login page

--- app/src/main/java/munik/androidprojects/tradefab/LoginPage.java
+++ app/src/main/java/munik/androidprojects/tradefab/LoginPage.java
@@ -1,7 +1,33 @@
 package munik.androidprojects.tradefab;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity{
+    public EditText mUsername,mPassword;
+    public Button mButton;
+    public TextView mSignup,mForgot_password;
+    public ProgressBar mProgressBar;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        mUsername=findViewById(R.id.username);
+        mPassword=findViewById(R.id.password);
+        mButton=findViewById(R.id.signin);
+        mSignup=findViewById(R.id.signup);
+        mForgot_password=findViewById(R.id.forgotpassword);
+        mProgressBar=findViewById(R.id.progressBar);
+
+        //handelingClicks
+
+    }
 }
