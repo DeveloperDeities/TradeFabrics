992a3f5b1ecd32dde094aac9a702a89596cd49d4
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-23 02:04:48 +0530

    added java code to   dialogbox which connects to its xml file

--- app/src/main/java/munik/androidprojects/tradefab/ShowingDialogForChoice.java
+++ app/src/main/java/munik/androidprojects/tradefab/ShowingDialogForChoice.java
@@ -1,4 +1,44 @@
 package munik.androidprojects.tradefab;
 
-class ShowingDialogForChoice {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.fragment.app.DialogFragment;
+
+public class ShowingDialogForChoice extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater =
+                getActivity().getLayoutInflater();
+        View dialogView =
+                inflater.inflate(R.layout.show_dialog, null);
+        final ImageView merchant_image=(ImageView)dialogView.findViewById(R.id.merchant);
+        final ImageView normal_image=(ImageView)dialogView.findViewById(R.id.normal);
+        builder.setView(dialogView);
+        merchant_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        normal_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        return builder.create();
+    }
 }
