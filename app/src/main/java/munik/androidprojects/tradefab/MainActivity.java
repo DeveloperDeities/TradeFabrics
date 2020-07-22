20ed765d315cb8a14af04f9858d0b6b3032d5cb4
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-23 03:34:57 +0530

    added welcomebye id to java code mainactivity

--- app/src/main/java/munik/androidprojects/tradefab/MainActivity.java
+++ app/src/main/java/munik/androidprojects/tradefab/MainActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    public TextView welcomebye;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Timer time=new Timer();
+        welcomebye=findViewById(R.id.welcomebyetext);
         time.schedule(new TimerTask(){
 
             @Override
