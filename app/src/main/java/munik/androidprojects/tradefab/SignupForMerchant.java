fbd2786a7aced8ef4f0ad6e5f508afa1f650fce1
Inspiron <nityanandasamanta96@gmail.com>
2020-07-23 20:21:01 +0530

    backgroundColor

---
+++ app/src/main/java/munik/androidprojects/tradefab/SignupForMerchant.java
@@ -0,0 +1,14 @@
+package munik.androidprojects.tradefab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupForMerchant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_for_merchant);
+    }
+}
