be89d94feaeb45b3c23460b9983df5efea2974dd
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-25 02:53:21 +0530

    animated button on click and progress bar in forgot password

--- app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
+++ app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
@@ -1,13 +1,27 @@
 package munik.androidprojects.tradefab;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ForgotPassword extends AppCompatActivity {
+    Button resetpass;
+    ProgressBar progressBarforgotpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        resetpass=findViewById(R.id.otpForgotPassGen);
+        progressBarforgotpass=findViewById(R.id.progressBarForgotpassk);
+        resetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //page to link  for otp verification
+                progressBarforgotpass.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
