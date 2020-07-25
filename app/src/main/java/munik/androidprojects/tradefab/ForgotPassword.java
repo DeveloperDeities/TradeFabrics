c158ec7169bc5c80ae276a6342873d7139d9fe2a
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-26 01:59:51 +0530

    added input from edittext mail to send verification link to

--- app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
+++ app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -16,15 +17,22 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
     Button resetpass;
+    EditText username;
+    EditText phoneno;
+    EditText email;
     ProgressBar progressBarforgotpass;
     FirebaseAuth auth = FirebaseAuth.getInstance();
-    String emailAddress = "user@example.com";
+    String emailAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
         resetpass=findViewById(R.id.otpForgotPassGen);
         progressBarforgotpass=findViewById(R.id.progressBarForgotpassk);
+        username = findViewById(R.id.name_forgot_password);
+        phoneno = findViewById(R.id.phone_forgot_password);
+        email = findViewById(R.id.email_forgot_password);
+        emailAddress = email.getText().toString();
         resetpass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
