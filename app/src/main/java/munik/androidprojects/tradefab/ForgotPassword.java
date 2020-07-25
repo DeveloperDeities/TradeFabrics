0b9b600c34c72f681203d251b7fc07997cce80ee
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-26 03:01:06 +0530

    added input from edittext mail to send verification link to mail id via fixing some old bugs

--- app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
+++ app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
@@ -1,7 +1,7 @@
 package munik.androidprojects.tradefab;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,8 +21,8 @@ public class ForgotPassword extends AppCompatActivity {
     EditText phoneno;
     EditText email;
     ProgressBar progressBarforgotpass;
-    FirebaseAuth auth = FirebaseAuth.getInstance();
     String emailAddress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,23 +32,32 @@ public class ForgotPassword extends AppCompatActivity {
         username = findViewById(R.id.name_forgot_password);
         phoneno = findViewById(R.id.phone_forgot_password);
         email = findViewById(R.id.email_forgot_password);
-        emailAddress = email.getText().toString();
         resetpass.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //page to link  for otp verification
-                //this sends mail to user for resetting the password
-                auth.sendPasswordResetEmail(emailAddress)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(ForgotPassword.this, "verification link sent", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+            public void onClick(View v) {emailAddress = email.getText().toString().trim();
                 progressBarforgotpass.setVisibility(View.VISIBLE);
+                sendmail();
             }
         });
+    }
+
+    private void sendmail() {
+        //page to link  for otp verification
+        //this sends mail to user for resetting the password
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // do something when mail was sent successfully.
+                            Toast.makeText(ForgotPassword.this, "recovery mail sent", Toast.LENGTH_SHORT).show();
+                            progressBarforgotpass.setVisibility(View.INVISIBLE);
+                        } else {
+                            // ...
+                        }
+                    }
+                });
     }
 }
