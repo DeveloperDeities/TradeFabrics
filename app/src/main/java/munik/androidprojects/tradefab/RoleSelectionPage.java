92e0dee7e18eedd57b77d53388a375ebdbc3121b
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-26 04:50:51 +0530

    added image button clickable still not connected to page to open on selection

--- app/src/main/java/munik/androidprojects/tradefab/RoleSelectionPage.java
+++ app/src/main/java/munik/androidprojects/tradefab/RoleSelectionPage.java
@@ -5,19 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RoleSelectionPage extends AppCompatActivity {
-
+    ImageView merchantPageviewRequired;
+    ImageView buyerPageviewRequired;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role_selection_page);
+        merchantPageviewRequired=findViewById(R.id.merchantimagetosell);
+        buyerPageviewRequired=findViewById(R.id.userpageforbuying);
+        merchantPageviewRequired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toMerchantPage();
+            }
+        });
+        buyerPageviewRequired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toBuyerPage();
+            }
+        });
     }
+
+
     public void logout(View v){
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(),LoginPage.class));
         finish();
     }
+    public void toBuyerPage(){}
+    public void toMerchantPage(){}
 }
