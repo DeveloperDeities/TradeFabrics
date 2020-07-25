9c4a345a1bca9e976940df843a87ca43255c6150
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-26 01:55:08 +0530

    added mailing feature on forget password presseed that generates a link on email for verifcation

--- app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
+++ app/src/main/java/munik/androidprojects/tradefab/ForgotPassword.java
@@ -1,15 +1,24 @@
 package munik.androidprojects.tradefab;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
     Button resetpass;
     ProgressBar progressBarforgotpass;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    String emailAddress = "user@example.com";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,16 @@ public class ForgotPassword extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //page to link  for otp verification
+                //this sends mail to user for resetting the password
+                auth.sendPasswordResetEmail(emailAddress)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ForgotPassword.this, "verification link sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                 progressBarforgotpass.setVisibility(View.VISIBLE);
             }
         });
