6247546bb736d5433601f3feb59f2e1d74a52b9b
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-26 05:01:30 +0530

    added xml files for buyer and seller pages but blank

--- app/src/main/java/munik/androidprojects/tradefab/BuyerPage.java
+++ app/src/main/java/munik/androidprojects/tradefab/BuyerPage.java
@@ -1,4 +1,15 @@
 package munik.androidprojects.tradefab;
 
-class BuyerPage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class BuyerPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_page);
+
+
+    }
 }
--- app/src/main/java/munik/androidprojects/tradefab/SellerPage.java
+++ app/src/main/java/munik/androidprojects/tradefab/SellerPage.java
@@ -1,4 +1,13 @@
 package munik.androidprojects.tradefab;
 
-class SellerPage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class SellerPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_page);
+    }
 }
