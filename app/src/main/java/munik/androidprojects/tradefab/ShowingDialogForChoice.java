38ac570fd231864c65c7307552d06b2478dcfd8c
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-07-26 05:32:51 +0530

    changed names

--- app/src/main/java/munik/androidprojects/tradefab/ShowingDialogForChoice.java
+++ app/src/main/java/munik/androidprojects/tradefab/ShowingDialogForChoice.java
@@ -3,7 +3,6 @@ package munik.androidprojects.tradefab;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,24 +17,21 @@ public class ShowingDialogForChoice extends DialogFragment {
                 new AlertDialog.Builder(getActivity());
         LayoutInflater inflater =
                 getActivity().getLayoutInflater();
-        View dialogView =
-                inflater.inflate(R.layout.show_dialog, null);
-        final ImageView merchant_image=(ImageView)dialogView.findViewById(R.id.merchant);
-        final ImageView normal_image=(ImageView)dialogView.findViewById(R.id.normal);
+        View dialogView = inflater.inflate(R.layout.show_dialog, null);
+        final ImageView merchant_image=(ImageView)dialogView.findViewById(R.id.merchantSelection);
+        final ImageView user_image=(ImageView)dialogView.findViewById(R.id.userSelection);
         builder.setView(dialogView);
         merchant_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LoginPage callingActivity = (
-                        LoginPage) getActivity();
+                LoginPage callingActivity = (LoginPage) getActivity();
                 callingActivity.take_to_Merchant_Login();
             }
         });
-        normal_image.setOnClickListener(new View.OnClickListener() {
+        user_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LoginPage callingActivity = (
-                        LoginPage) getActivity();
+                LoginPage callingActivity = (LoginPage) getActivity();
                 callingActivity.take_to_buyer_Login();
             }
         });
